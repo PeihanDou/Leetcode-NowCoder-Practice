22f6b9379940fc1e482cc637b4d56a9664755156
PeihanDou <55855936+PeihanDou@users.noreply.github.com>
2020-10-13 20:55:56 -0400

    sort ag 2020 10 13

--- sort_package/sort.py
+++ sort_package/sort.py
@@ -1,4 +1,4 @@
-n = [1,4,2,5,7,6,3,4,8,7,4,6,9]
+n = [5,3,2,6,4,1]
 
 def switch_sort(nums):
     for i in range(len(nums)-1):
@@ -44,5 +44,55 @@ def merge(l, r):
             m.append(r.pop(0))
     if l:
         m += l
+    if r:
+        m += r
+    return m
+
+def quicksort(nums, l, h):
+    if l < h:
+        p = partition(nums, l, h)
+        quicksort(nums, l, p-1)
+        quicksort(nums, p+1, h)
+    return nums
+    
+def partition(nums, l, h):
+    pivot = nums[h]
+    i = l-1
+    # 保证i之前的都是比pivot小的
+    for j in range(l, h):
+        if nums[j] < pivot:
+            i += 1
+            nums[i], nums[j] = nums[j], nums[i]
+        
+    nums[h], nums[i+1] = nums[i+1], nums[h]
+    return i+1
+
+def heapsort(n):
+    n = buildMaxheap(n)
+    l = len(n)-1
+    
+    for i in range(len(n)-1, -1, -1):
+        n[i], n[0] = n[0], n[i]
+        l -= 1
+        n = heapify(n, 0, l)
+    return n
+
+def heapify(n, i, length): # 上浮
+    left = n[2*i+1] if 2*i+1 <length else float('-inf')
+    right = n[2*i+2] if 2*i+2 <length else float('-inf')
+    if n[i] < left or n[i] < right:
+        h_id = 2*i+1 if left > right else 2*i+2
+        n[h_id], n[i] = n[i], n[h_id]
+        heapify(n, h_id, length)
+    return n
+
+def buildMaxheap(n):
+    for i in range(len(n)//2, -1, -1):
+        n = heapify(n, i, len(n))
+    return n
+
+
+    
 # test
-print(select_sort(n))
+print(n)
+print(heapsort(n))
