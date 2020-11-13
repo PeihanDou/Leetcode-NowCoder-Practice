291111ae7706eb41974e2c768c84431d20e01827
PeihanDou <douph@umich.edu>
2020-11-12 22:04:42 -0500

    2020 11 12

--- jz23.py
+++ jz23.py
@@ -16,7 +16,10 @@ def isBST(seq, start, end):
     pivot = start
     while pivot < end and seq[pivot] < seq[end]:
         pivot += 1
+    # while结束时，[:pivot]必定是左子树(都比end小), [pivot:end-1]可能是右子树
+    # 接下来的if检测[pivot:end]是不是右子树
     if min(seq[pivot:end+1]) < seq[end]: return False # 右子树有比end小的数， 不是BST 或者 左子树没遍历完就碰到比end大的数， 同样不是BST
+    # 是右子树，则分别检测两个子树是不是BST
     return isBST(seq, start, pivot-1) and isBST(seq, pivot, end-1)
 
 # 单调栈 O(n)
@@ -25,6 +28,11 @@ def isBST(seq, start, end):
 # 若遇见小的， 说明是左子树（可能是当前节点的左子树，也可能是父辈节点的左子树），这是因为左子树必有max的约束
 # 循环弹栈（就是说，在右子树上不断向根走去，知道到达这个左子树为之根的那个节点），设为max约束
 # 如果大于mmax则False，不能形成二叉树
+
+# 将入栈操作理解为一个指针指向其右孩子
+# 弹栈就是回到父节点的过程
+
+# 试着写出前序遍历的版本？
 def VerifySquenceOfBST1(self, sequence):
     # write code here
     if not sequence: return False
@@ -37,4 +45,6 @@ def VerifySquenceOfBST1(self, sequence):
             mmax = stack.pop()
         stack.append(s)
     return True
+
+
 
